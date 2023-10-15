38708d6578dd6214b39efb8ac312840a29c2f9fd
Roman Matviy <roman@matviy.pp.ua>
2023-10-15 19:58:58 +0300

    update

---
+++ import.php
@@ -0,0 +1,33 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Get the email address from the form
+    $email = $_POST['email'];
+
+    // Define the recipient email address
+    $recipient = "info@apartner.pro";
+
+    // Set the subject of the email
+    $subject = "New subscription from your website";
+
+    // Compose the email message
+    $message = "You have a new subscriber with the email address: " . $email;
+
+    // Set the headers for the email
+    $headers = "From: " . $email . "\r\n" .
+               "Reply-To: " . $email . "\r\n" .
+               "X-Mailer: PHP/" . phpversion();
+
+    // Send the email
+    if (mail($recipient, $subject, $message, $headers)) {
+        // Email sent successfully
+        echo "Thank you for subscribing!";
+    } else {
+        // Email sending failed
+        echo "Oops! There was an error while subscribing. Please try again later.";
+    }
+} else {
+    // Redirect users to the form page if they try to access this script directly
+    header("Location: https://teampresta.com");
+    exit();
+}
+?>
