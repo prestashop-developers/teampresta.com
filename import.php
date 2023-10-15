b57c325767971a8a465244ec0ba4316b8f5e6c25
Roman Matviy <roman@matviy.pp.ua>
2023-10-15 21:12:06 +0300

    update

--- import.php
+++ import.php
@@ -10,13 +10,17 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         // Set the subject of the email
         $subject = "New subscription from your website";
 
-        // Compose the email message
-        $message = "You have a new subscriber with the email address: " . $email;
-        $message .= '<a hreh="https://teampresta.com">TEAM PRESTA</a><br>';
-        $message .= '<a hreh="https://apartner.pro">APARTNER.PRO</a><br>';
+        // Compose the email message as HTML
+        $message = '<html><body>';
+        $message .= '<p>You have a new subscriber with the email address: ' . $email . '</p>';
+        $message .= '<a href="https://teampresta.com">TEAM PRESTA</a><br>';
+        $message .= '<a href="https://apartner.pro">APARTNER.PRO</a><br>';
+        $message .= '</body></html>';
 
         // Set the headers for the email
-        $headers = "From: " . $email . "\r\n" .
+        $headers = "MIME-Version: 1.0" . "\r\n";
+        $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+        $headers .= "From: " . $email . "\r\n" .
                 "Reply-To: " . $email . "\r\n" .
                 "X-Mailer: PHP/" . phpversion();
 
